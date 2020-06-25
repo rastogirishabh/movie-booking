41792a7a3a0b3be8a39cf3459ff4665659a80eb0
rastogi <rastogi@rastogi-PC>
2020-06-26 01:52:41 +0530

    In memory database implemented.

--- movie-booking/src/main/java/com/codejudge/moviebooking/service/MovieServiceImpl.java
+++ movie-booking/src/main/java/com/codejudge/moviebooking/service/MovieServiceImpl.java
@@ -1,12 +1,10 @@
 package com.codejudge.moviebooking.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codejudge.moviebooking.dao.TemporaryRepository;
 import com.codejudge.moviebooking.exceptions.MovieAlreadyPresentException;
 import com.codejudge.moviebooking.requestmodel.MovieRequestModel;
 import com.codejudge.moviebooking.responsemodel.MovieResponseModel;
@@ -17,21 +15,22 @@ public class MovieServiceImpl implements MovieService {
 	@Autowired
 	MovieUtils movieUtils;
 	
-	List<MovieResponseModel> movieList = new ArrayList<>();
+	@Autowired
+	TemporaryRepository database;
 
 	@Override
 	public MovieResponseModel createMovie(MovieRequestModel movieDetailsReceived) {
 		
 		MovieResponseModel movieCreated = new MovieResponseModel();
 		
-		if(movieUtils.isMoviePresent(movieDetailsReceived.getMovie_name(), movieList)) {
+		if(movieUtils.isMoviePresent(movieDetailsReceived.getMovie_name(), database.getMovieList())) {
 			throw new MovieAlreadyPresentException("Movie name : " + movieDetailsReceived.getMovie_name());
 		}
 		
 		BeanUtils.copyProperties(movieDetailsReceived, movieCreated);
 		movieCreated.setMovie_id(movieUtils.generateMovieID());
 		
-		movieList.add(movieCreated);
+		database.movieList.add(movieCreated);
 		
 		return movieCreated;
 	}
--- movie-booking/src/main/java/com/codejudge/moviebooking/service/TheatreServiceImpl.java
+++ movie-booking/src/main/java/com/codejudge/moviebooking/service/TheatreServiceImpl.java
@@ -1,12 +1,10 @@
 package com.codejudge.moviebooking.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codejudge.moviebooking.dao.TemporaryRepository;
 import com.codejudge.moviebooking.requestmodel.TheatreRequestModel;
 import com.codejudge.moviebooking.responsemodel.TheatreResponseModel;
 import com.codejudge.moviebooking.utils.TheatreUtils;
@@ -16,21 +14,23 @@ public class TheatreServiceImpl implements TheatreService {
 	@Autowired
 	TheatreUtils theatreUtils;
 	
-	private List<TheatreResponseModel> theatreList = new ArrayList<>();
+
+	@Autowired
+	TemporaryRepository database;
 
 	@Override
 	public TheatreResponseModel addTheatre(TheatreRequestModel theatreInputDetails) {
 		
 		TheatreResponseModel theatreAdded = new TheatreResponseModel();
 		
-		if(theatreUtils.isTheatreRegistered(theatreInputDetails.getTheatre_name(), theatreInputDetails.getCity(), theatreList)) {
+		if(theatreUtils.isTheatreRegistered(theatreInputDetails.getTheatre_name(), theatreInputDetails.getCity(), database.getTheatreList())) {
 			throw new TheatreAlreadyRegisteredException("Theatre with name : " 
 		+theatreInputDetails.getTheatre_name() + " already registered in city : " + theatreInputDetails.getCity()); 
 		}
 		BeanUtils.copyProperties(theatreInputDetails, theatreAdded);
 		theatreAdded.setTheatre_id(theatreUtils.generateTheatreId());
 		
-		theatreList.add(theatreAdded);
+		database.theatreList.add(theatreAdded);
 		
 		return theatreAdded;
 	}
