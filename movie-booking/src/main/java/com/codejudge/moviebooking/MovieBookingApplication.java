6e10918ac18df158fd7bf3df7814447b3974c809
Rishabh Rastogi <rishabhrastogi@live.com>
2021-01-11 22:58:35 +0530

    Configuration added for tomcat container deployment

--- movie-booking/src/main/java/com/codejudge/moviebooking/MovieBookingApplication.java
+++ movie-booking/src/main/java/com/codejudge/moviebooking/MovieBookingApplication.java
@@ -2,13 +2,20 @@ package com.codejudge.moviebooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class MovieBookingApplication {
+public class MovieBookingApplication extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieBookingApplication.class, args);
 		
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieBookingApplication.class);
 	}
 
 }
